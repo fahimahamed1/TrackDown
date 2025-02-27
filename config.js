b6abbf177cfde4b8550bc77a8d1b92f7cd685e76
Fahim Ahamed <121462098+fahimahamed1@users.noreply.github.com>
2025-02-27 20:34:51 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
    botToken: "7674899297:AAGgT_MT8BqftptvyRkro4M9rECrVCYd4mo", // Modify your bot TOKEN here
-   hostURL: "https://open-5a79.onrender.com", // Modify your host URL here
+   hostURL: "https://open-cemn.onrender.com", // Modify your host URL here
 };
 // for port forewording use "ssh -R 80:localhost:5000 serveo.net"
